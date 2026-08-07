f43c782b2d17519b34e2ed9b3ee06a2f63972bd0
apple <apple@apples-MacBook-Air.local>
2026-08-07 17:31:19 +0900

    fix: 코드래빗 리뷰 반영

--- src/api/adminEstimateRequestPaths.ts
+++ src/api/adminEstimateRequestPaths.ts
@@ -1,7 +1,4 @@
 export const ADMIN_ESTIMATE_REQUEST_LIST_PATHS = '/api/admin/estimate-requests';
 export const ADMIN_ESTIMATE_REQUEST_STATISTICS_PATHS = `${ADMIN_ESTIMATE_REQUEST_LIST_PATHS}/statistics`;
-export const getAdminEstimateRequesDetail = async (
-  estimateRequestId: number
-) => {
-  return `${ADMIN_ESTIMATE_REQUEST_LIST_PATHS}/${estimateRequestId}`;
-};
+export const getAdminEstimateRequestDetailPath = (estimateRequestId: number) =>
+  `${ADMIN_ESTIMATE_REQUEST_LIST_PATHS}/${estimateRequestId}`;
